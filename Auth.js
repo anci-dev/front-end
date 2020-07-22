3fc67232621bfd6e6ce2ff11c68c79f849406cf1
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2020-07-22 17:16:10 -0400

    Store auth data in cookie; auto-login if auth cookie exists

--- Auth.js
+++ Auth.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GitHubLogin from 'react-github-login';
 import PopupWindow from 'react-github-login/src/PopupWindow'
-import { TouchableOpacity, Text } from 'react-native';
+import { TouchableOpacity, Text, ActivityIndicator } from 'react-native';
 import styles from './static/Style'
 
+const Cookies = require('js-cookie');
+const COOKIE_OPTIONS = {
+    expires: 1, // Auth cookie expires after 1 day
+}
 
-function getAuth(setAuth) {
+function getAuth(setAuth, setProcessing) {
+    setProcessing(true);
+
     const query = "client_id=13f6352ecd93ee690191&redirectUri=http://localhost:3000/auth/get_code"
     const url = "https://github.com/login/oauth/authorize?" + query;
     const tab = window.open(url);
@@ -16,21 +22,28 @@ function getAuth(setAuth) {
     function recieveMessage(event) {
         if (event.origin !== "http://localhost:3000") {
             console.log("Wrong domain!");
-
         } else {
             tab.close();
             // close listener when message recieved
             window.removeEventListener("message", recieveMessage, false);
 
-            // add hooks here
-            setAuth({...JSON.parse(event.data), success: true});
+            // Store auth data. Propagate the change directly and store it for later.
+            let auth = {...JSON.parse(event.data), success: true};
+            Cookies.set("auth", auth, COOKIE_OPTIONS);
+            console.log(auth);
+            setAuth(auth);
+            setProcessing(false);
         }
     }
 }
 
 function LoginButton(props) {
-    return (
-        <TouchableOpacity style={styles.buttonContainer} onPress={() => getAuth(props.setAuth)}>
+    const [processing, setProcessing] = useState(false);
+
+    return processing ? (
+        <ActivityIndicator size="large" color="#2a70f0" style={{margin: 25}}/>
+    ) : (
+        <TouchableOpacity style={styles.buttonContainer} onPress={() => getAuth(props.setAuth, setProcessing)}>
             <Text style={styles.buttonText}>Log in</Text>
         </TouchableOpacity>
     );
--- views/Home.js
+++ views/Home.js
@@ -1,26 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { TouchableOpacity, StyleSheet, Text, View, Image } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { TouchableOpacity, StyleSheet, Text, View, Image, Animated, Easing } from 'react-native';
 import styles from '../static/Style'
 import { LoginButton } from '../Auth'
 import logo from '../static/images/logo1.png'
 
+const Cookies = require('js-cookie');
 
 export function Home({ navigation }) {
     // Hand off setAuth to the login handler. Have it update when oauth is over.
-    const [auth, setAuth] = useState({});
+    const [auth, setAuth] = useState(Cookies.getJSON("auth") || {});
 
     useEffect(() => {
         if(auth.success) navigation.navigate("Authenticated", {auth});
-        // Eventually, something should happen is the auth attempt is unsuccessful.
+        // Eventually, something should probably happen if the auth attempt is unsuccessful.
     }, [auth]);
+
+    // Smoothly display the login screen
+    const fade = useRef(new Animated.Value(0)).current
+    useEffect(() => {
+        Animated.timing(fade, {
+            toValue: 1,
+            duration: 750,
+            easing: Easing.cubic,
+        }).start();
+    }, [fade]);
 
     return (
         <View style={styles.base}>
-            <View style={styles.container}>
-                <Text style={styles.title}>Welcome to anci!</Text>
-                <LoginButton setAuth={setAuth} />
-                <Image source={logo} style={styles.logo}/>
-            </View>
+            <Animated.View style={[styles.base, {opacity: fade}]}>
+                <View style={styles.container}>
+                    <Text style={styles.title}>Welcome to anci!</Text>
+                    <LoginButton setAuth={setAuth} />
+                    <Image source={logo} style={styles.logo}/>
+                </View>
+            </Animated.View>
         </View>
     );
 }
