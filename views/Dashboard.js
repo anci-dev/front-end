b8a3584ff8b12a8365d42c1df7e4c23af67de495
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2020-07-22 14:17:52 -0400

    Swap stylesheet declaration to compose (rather than create)

--- views/Dashboard.js
+++ views/Dashboard.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import globalStyle from '../static/Style'
 
 export function Dashboard(props) {
-    
+
     return (
         <View>
             <View style={styles.container}>
@@ -14,4 +14,4 @@ export function Dashboard(props) {
 }
 
 // {...globalStyle, style dict specific to this component}
-const styles = StyleSheet.create(globalStyle);
+const styles = StyleSheet.compose(globalStyle);
